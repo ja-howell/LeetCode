2d0520534ffb0d3f5517744276367fd72f879ee3
Jami Howell <jami.howell0@gmail.com>
2024-02-18 10:56:08 -0600

    Add Palindrome Number Solution

---
+++ PalindromeNumber/Solution.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+public class Solution{
+    public bool IsPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        List<int> digits = new List<int>();
+        while (x != 0) {
+            int y = x % 10;
+            x = x / 10;
+            digits.Add(y);
+        }
+        for (int i = 0; i < digits.Count / 2; i++) {
+            if (digits[i] != digits[digits.Count - i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
