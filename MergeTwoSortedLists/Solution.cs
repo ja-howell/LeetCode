72512de9f003df54004f9e6bbb75160a16fae1b3
Jami Howell <jami.howell0@gmail.com>
2024-02-20 20:20:31 -0600

    Start Merge Two Sorted Lists

---
+++ MergeTwoSortedLists/Solution.cs
@@ -0,0 +1,18 @@
+//We're comparing the first two numbers, taking the lesser one and putting it in a new list, taking the greater and storeing it.
+// Take the stored value and compare to next item on the lesser one's list, etc...
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        return list1;
+    }
+}
