7f1234f3da1c28eeb7f5350dbde431f7fc67bb62
Jami Howell <jami.howell0@gmail.com>
2024-02-23 21:36:11 -0600

    Add Remove Duplicates from Sorted Away

---
+++ 0026RemoveDuplicates/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        // Example usage:
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        Solution solution = new Solution();
+        int k = solution.RemoveDuplicates(nums);
+
+        for (int i = 0; i < k; i++) {
+            Console.WriteLine(nums[i]);
+        }
+        Console.WriteLine("k: " + k);
+    }
+}
