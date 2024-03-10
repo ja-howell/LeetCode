39e19f2fbafc8d82d652c70051296076c75411f7
Jami Howell <jami.howell0@gmail.com>
2024-03-10 09:17:20 -0500

    Adds Apple Redistribution Solution

---
+++ 3074AppleRedistribution/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] apple = {1, 3, 2};
+        int[] capacity = {4, 3, 1, 5, 2};
+
+        Solution solution = new Solution();
+        int result = solution.MinimumBoxes(apple, capacity);
+        Console.WriteLine(result);
+    }
+}
