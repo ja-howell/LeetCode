72a06a0b27b427469dc026c920bf7e7d8050e767
Jami Howell <jami.howell0@gmail.com>
2024-03-10 09:25:49 -0500

    Adds solution for problem 3075

---
+++ 3075MaximizeHappiness/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] happiness = { 2, 3, 4, 5 };
+        int k = 1;
+
+        Solution solution = new Solution();
+        long result = solution.MaximumHappinessSum(happiness, k);
+        Console.WriteLine(result);
+    }
+}
