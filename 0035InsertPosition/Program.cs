29f7b32b124b02939badbd9caac209f33692d19c
Jami Howell <jami.howell0@gmail.com>
2024-03-10 19:39:02 -0500

    Adds solution for problem 0035

---
+++ 0035InsertPosition/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = {1, 3, 5, 6};
+        int target = 7;
+
+        Solution solution = new Solution();
+        int result = solution.SearchInsert(nums, target);
+        Console.WriteLine(result);
+    }
+}
---
+++ 0035InsertPosition/Solution.cs
@@ -0,0 +1,40 @@
+public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        int begin = 0;
+        int end = nums.Length;
+        return BinarySearch(nums, target, begin, end);
+
+    }
+
+    private int BinarySearch(int[] nums, int target, int begin, int end)
+    {
+        int mid = (begin + end) / 2;
+        if (mid >= nums.Length)
+        {
+            return mid;
+        }
+        if (target == nums[mid])
+        {
+            return mid;
+        }
+        if (begin == end && nums[mid] < target)
+        {
+            return mid + 1;
+        }
+        if (begin == end && nums[mid] > target)
+        {
+            return mid;
+        }
+        if (target < nums[mid])
+        {
+            return BinarySearch(nums, target, begin, mid);
+        }
+        else if (target > nums[mid])
+        {
+            return BinarySearch(nums, target, mid + 1, end);
+        }
+        return mid;
+    }
+}
