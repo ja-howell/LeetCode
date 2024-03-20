1e6642a198881aba7e445e26b5d65c432ab17d51
Jami Howell <jami.howell0@gmail.com>
2024-03-19 19:36:46 -0500

    Adds 0067 solution

---
+++ 0067AddBinary/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string a = "1010";
+        string b = "1011";
+        Solution solution = new Solution();
+        string result = solution.AddBinary(a,b);
+        Console.WriteLine(result);
+    }
+}
