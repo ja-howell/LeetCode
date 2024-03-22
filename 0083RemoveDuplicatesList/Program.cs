1958cf62da9236e727262e60f201ebeb2af64831
Jami Howell <jami.howell0@gmail.com>
2024-03-21 19:49:01 -0500

    Switches 0083 Program.cs to table driven tests

--- 0083RemoveDuplicatesList/Program.cs
+++ 0083RemoveDuplicatesList/Program.cs
@@ -4,14 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        ListNode? test1 = new ListNode(4);
-        ListNode.append(ref test1, 1);
-        ListNode.append(ref test1, 1);
-        ListNode.append(ref test1, 1);
-        ListNode.append(ref test1, 5);
-
+        int[][] testCases = {
+            new int[] { 1, 2, 2, 2, 2, 3, 3, 4, 5, 5, 5, 6 },
+            new int[] { 1, 2, 3, 4, 5, 5, 5, 6 },
+            new int[] { }
+        };
         Solution solution = new Solution();
-        ListNode? result = solution.DeleteDuplicates(test1);
-        ListNode.print(ref result);
+        foreach (int[] testCase in testCases)
+        {
+            ListNode? test1 = null;
+            foreach (var num in testCase)
+            {
+                ListNode.append(ref test1, num);
+            }
+
+            ListNode? result = solution.DeleteDuplicates(test1);
+            ListNode.print(ref result);
+        }
     }
 }
