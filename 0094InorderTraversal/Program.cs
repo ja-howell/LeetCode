edd7f95033d776f63092753adfab3e03feb75b38
Jami Howell <jami.howell0@gmail.com>
2024-03-24 21:15:01 -0500

    Adds 0094 solution

---
+++ 0094InorderTraversal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution.TreeNode t = new Solution.TreeNode(
+            1,
+            null, // left
+            new Solution.TreeNode(
+                2,
+                new Solution.TreeNode( // left
+                    3,
+                    new Solution.TreeNode(5, null, null), // left
+                    new Solution.TreeNode(6, null, null) // right
+                ),
+                new Solution.TreeNode( // right
+                    4,
+                    new Solution.TreeNode(7, null, null), // left
+                    new Solution.TreeNode(8, null, null)  // right
+                )
+            )
+        );
+
+        Solution solution = new Solution();
+        IList<int> result = solution.InorderTraversal(t);
+        foreach (int num in result)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
