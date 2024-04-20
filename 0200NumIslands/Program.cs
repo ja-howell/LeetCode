17d18b248016958a72e263d6219c449f24d567bc
Jami Howell <42947215+ja-howell@users.noreply.github.com>
2024-04-19 19:56:06 -0500

    Adds solution for 0200

---
+++ 0200NumIslands/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char[][] grid = new char[][] 
+        {
+            new char[] { '1', '1', '1', '1', '0' },
+            new char[] { '1', '1', '0', '1', '0' },
+            new char[] { '1', '1', '0', '0', '0' },
+            new char[] { '0', '0', '0', '0', '0' }
+        };
+
+        Solution solution = new Solution();
+        Console.WriteLine(solution.NumIslands(grid));
+    }
+}
