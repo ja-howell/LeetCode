0472b29bd92f086358698d07791e10fe78bd2140
Jami Howell <jami.howell0@gmail.com>
2024-04-29 20:44:56 -0500

    Fails at big indexes

--- 0297SerializeDeserialize/Codec.cs
+++ 0297SerializeDeserialize/Codec.cs
@@ -8,13 +8,14 @@
  * }
  */
 #nullable enable
+using System.Text;
 public class Codec
 {
 
     // Encodes a tree to a single string.
     public string serialize(TreeNode root)
     {
-        string data = "";
+        StringBuilder sb = new();
         PriorityQueue<string,int> pqueue = new();
         SerializeHelper(root, pqueue);
         int lastIndex = 0;
@@ -23,87 +24,78 @@ public class Codec
             int index;
             string temp;
             pqueue.TryPeek(out temp, out index);
-            Console.WriteLine($"i: {index}, val: {temp}");
-            //Add index - lastIndex - 1 n's
-            for (int i = 0; i < (index - lastIndex - 1); i++)
-            {
-                data += "n,";
-            }
-            data += pqueue.Dequeue();
-            lastIndex = index;
+            pqueue.Dequeue();
+            sb.Append($"{index}:{temp}");
             if (pqueue.Count > 0)
             {
-                data += ',';
+                sb.Append(',');
             }
+            lastIndex = index;
         }
-        data.TrimEnd(',');
-        Console.WriteLine(data);
-        return data;
+        return sb.ToString();
     }
 
-    private void SerializeHelper(TreeNode root, PriorityQueue<string,int> pqueue, int i=0)
+    private static void SerializeHelper(TreeNode root, PriorityQueue<string,int> pqueue, int i=0)
     {
         if (root is null)
         {
             return;
         }
+        Console.WriteLine($"({root.val.ToString()}, {i})");
         pqueue.Enqueue(root.val.ToString(), i);
         SerializeHelper(root.left, pqueue, 2 * i + 1);
         SerializeHelper(root.right, pqueue, 2 * i + 2);
     }
 
     // Decodes your encoded data to tree.
-    public TreeNode deserialize(string data)
+    public TreeNode? deserialize(string data)
     {
+        Console.WriteLine(data);
         if (data.Length == 0)
         {
             return null;
         }
-        string[] tree = data.Split(',');
-        
-        return DeserializeHelper(tree);
-    }
-    //Takes a string and if that string begins with n returns null, otherwise we create a node using the val
 
-//[1,2,3,null,null,4,5]
-    private TreeNode? DeserializeHelper(string[] data, int i = 0)
-    {
-        //TODO: Split data on commas create an array.
-        if (i >= data.Length)
+        Dictionary<int, TreeNode> indexNodeMapping = new();
+
+        string[] keyValuePairs = data.Split(',');
+
+        int index;
+        int val;
+        (index, val) = SplitKeyValuePair(keyValuePairs[0]);
+        indexNodeMapping[index] = new TreeNode(val);
+
+        for (int i = 1; i < keyValuePairs.Length; i++)
         {
-            return null;
+            (index, val) = SplitKeyValuePair(keyValuePairs[i]);
+
+            // Console.WriteLine($"({index}, {val})");
+            indexNodeMapping[index] = new TreeNode(val);
+            int parent = Parent(index);
+
+            if (index % 2 == 1)
+            {
+                indexNodeMapping[parent].left = indexNodeMapping[index];
+            }
+            else
+            {
+                indexNodeMapping[parent].right = indexNodeMapping[index];
+            }
         }
-        if (data[i] == "n")
-        {
-            return null;
-        }
-        int val = int.Parse(data[i]);
-        TreeNode root = new TreeNode(val);
-        root.left = DeserializeHelper(data, i * 2 + 1);
-        root.right = DeserializeHelper(data, i * 2 + 2);
-        
-        return root;
+        return indexNodeMapping[0];
     }
 
-    private int chartoint(char a)
+    private (int, int) SplitKeyValuePair(string kv)
     {
-        return a - '0';
+        string[] parts = kv.Split(':');
+        return (int.Parse(parts[0]), int.Parse(parts[1]));
     }
 
-        private static int Left(int i)
+    private static int Parent(int i)
     {
-        return 2 * i + 1;
+        return (i - 1) / 2;
     }
 
-    private char IntToChar(int i)
-    {
-        return (char)(i + (int)'0');
-    }
-
-    private static int Right(int i)
-    {
-        return 2 * i + 2;
-    }
 }
 
 // Your Codec object will be instantiated and called as such:
