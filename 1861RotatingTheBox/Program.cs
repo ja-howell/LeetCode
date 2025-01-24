723d900d1aec2638bd7e06b35bd88296948d51c2
Jami Howell <jami.howell0@gmail.com>
2025-01-24 09:27:44 -0600

    Adds solution for 1861 from November...

---
+++ 1861RotatingTheBox/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        char[][] box = new char[][]
+        {
+            new char[] {'#','.','*','.'},
+            new char[] {'#','#','*','.'}
+        };
+
+        // for (int i = 0; i < box.Length; i++)
+        // {
+        //     Console.Write("[ ");
+        //     for (int j = 0; j < box[0].Length; j++)
+        //     {
+        //         Console.Write(box[i][j] + " ");
+        //     }
+        //     Console.WriteLine("]");
+        // }
+
+
+        char[][] rotatedBox = solution.RotateTheBox(box);
+        // Console.WriteLine("Rotated Box");
+
+        // for (int i = 0; i < rotatedBox.Length; i++)
+        // {
+        //     Console.Write("[ ");
+        //     for (int j = 0; j < rotatedBox[0].Length; j++)
+        //     {
+        //         Console.Write(rotatedBox[i][j] + " ");
+        //     }
+        //     Console.WriteLine("]");
+        // }
+    }
+}
