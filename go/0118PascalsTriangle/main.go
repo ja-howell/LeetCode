a58565eeb8eebbeb87270f56a5def8fb3ade23ac
jahowell <jami.howell0@gmail.com>
2025-09-08 17:03:14 -0500

    Adds partial code for 118

---
+++ go/0118PascalsTriangle/main.go
@@ -0,0 +1,11 @@
+func generate(numRows int) [][]int {
+    if numRows == 1 {
+        return [][]int{{1}}
+    }
+    row := []int{}
+    prevRow := []int{1,1}
+    triangle := [][]int{{1},{1,1}}
+    for i := 0, i <= numRows-2; i++ {
+        row = slices.append
+    }
+}
