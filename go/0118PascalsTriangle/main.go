e9fc33ad3ab5ca3b0def12a1930b508e62fa3b57
Jami Howell <jami.howell0@gmail.com>
2025-09-08 19:03:15 -0500

    Finishes solution for 118

--- go/0118PascalsTriangle/main.go
+++ go/0118PascalsTriangle/main.go
@@ -1,11 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(generate(5))
+}
+
 func generate(numRows int) [][]int {
-    if numRows == 1 {
-        return [][]int{{1}}
-    }
-    row := []int{}
-    prevRow := []int{1,1}
-    triangle := [][]int{{1},{1,1}}
-    for i := 0, i <= numRows-2; i++ {
-        row = slices.append
-    }
+	if numRows == 1 {
+		return [][]int{{1}}
+	}
+	prevRow := []int{1, 1}
+	triangle := [][]int{{1}, {1, 1}}
+	for i := 0; i < numRows-2; i++ {
+		row := buildRow(prevRow)
+		triangle = append(triangle, row)
+		prevRow = row
+	}
+	return triangle
+}
+
+func buildRow(prevRow []int) []int {
+	row := []int{1}
+	for j := 1; j < len(prevRow); j++ {
+		row = append(row, prevRow[j-1]+prevRow[j])
+	}
+	row = append(row, 1)
+	return row
 }
